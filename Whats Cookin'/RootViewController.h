99d2a70d770f4d02922a2f031c71ecc6e6d233c2
Sir Tidez <SirTidez@cobaltstudios.org>
2015-10-14 06:35:20 -0700

    Switched from default template

--- Whats Cookin'/RootViewController.h
+++ Whats Cookin'/RootViewController.h
@@ -2,15 +2,12 @@
 //  RootViewController.h
 //  Whats Cookin'
 //
-//  Created by Tyler Ludka on 10/13/15.
-//  Copyright (c) 2015 Cobalt Studios. All rights reserved.
+//  Created by Tyler Ludka on 10/14/15.
+//  Copyright © 2015 Cobalt Studios. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface RootViewController : UIViewController <UIPageViewControllerDelegate>
-
-@property (strong, nonatomic) UIPageViewController *pageViewController;
+@interface RootViewController : UIViewController
 
 @end
-
