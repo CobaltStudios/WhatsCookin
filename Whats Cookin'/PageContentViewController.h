0796ba8b28c5754b9f37001adfa7992c63e60883
Sir Tidez <SirTidez@cobaltstudios.org>
2015-10-14 06:51:51 -0700

    Began work on setting up page content

---
+++ Whats Cookin'/PageContentViewController.h
@@ -0,0 +1,16 @@
+//
+//  PageContentViewController.h
+//  Whats Cookin'
+//
+//  Created by Tyler Ludka on 10/14/15.
+//  Copyright © 2015 Cobalt Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PageContentViewController : UIViewController
+@property NSUInteger pageIndex;
+@property NSString *imgFile;
+@property NSString *txtTitle;
+
+@end
